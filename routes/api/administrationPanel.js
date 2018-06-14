1a2a8733c725e83c0fbbacdc59e462e376316ac9
JothaCortes <j.cortesolivarez@gmail.com>
2018-06-14 12:35:54 -0400

    actualizacion horarios

--- routes/api/administrationPanel.js
+++ routes/api/administrationPanel.js
@@ -33,6 +33,121 @@ const administrationPanel = [
 //Api editar valores de cuotas
 
 //xxxxx
+//Api Guardar Horarios
+ { 
+    method: 'POST',
+    path: '/api/nuevoHorario',
+    options: {
+        handler: (request, h) => {
+            let year  = request.payload.año;
+            let horary= request.payload.horario;
+            let horaryObject = {
+                _id: moment.tz('America/Santiago').format('YYYY-MM-DDTHH:mm:ss.SSSSS'),
+                type  : 'horarios',
+                status: 'enabled',
+                year  :year, 
+                horary:horary
+            }
+            return new Promise(resolve => {
+                db.insert(horaryObject, function (errUpdate, body) {
+                    if (errUpdate) throw errUpdate;
+                    resolve({ ok: 'Horario ' + horaryObject.horary + ' agregado correctamente' });
+                });
+            }) 
+        },
+        validate: {
+            payload: Joi.object().keys({
+                año: Joi.string().allow(''),
+                horario: Joi.string().allow('')
+            })
+        }
+    }
+},
+//xxxxx
+//Obtener Horarios para dataTable
+{ 
+    method: 'GET',
+    path: '/api/horariosCebal', 
+    options: {
+        handler: (request, h) => {
+            return new Promise(resolve => {
+                db.find({
+                    'selector': {
+                        '_id': {
+                            '$gte': null
+                        },
+                        'type': 'horarios',
+                    }
+                }, (err, result) => {
+                    if (err) throw err;
+
+                    if (result.docs[0]) {
+                        let res = result.docs.reduce((arr, el, i)=>{
+                            return arr.concat({
+                                _id: el._id,
+                                year: el.year,
+                                horary: el.horary
+                            })
+                        }, []) 
+
+                        resolve(res);
+                    } else {
+                        resolve({ err: 'no existen horarios' });
+                    }
+                });
+            });
+        }
+    }
+}, 
+//Modificar los horarios
+{
+    method: 'POST',
+    path: '/api/modHorary',
+    options: {
+        handler: (request, h) => {
+            let id = request.payload.id;
+            let year = request.payload.year;
+            let horary = request.payload.horary;
+            let modHorariosObj = {};
+
+            return new Promise(resolve => {
+                db.find({
+                    "selector": {
+                        "_id": id,
+                        "type": "horarios",
+                        "status": "enabled"
+                    },
+                    "limit": 1
+                }, function (err, result) {
+                    if (err) throw err;
+
+                    if (result.docs[0]) {
+                        modHorariosObj = result.docs[0];
+                        modHorariosObj.year = year;
+                        modHorariosObj.horary = horary;
+
+                        db.insert(modHorariosObj, function (errUpdate, body) {
+                            if (errUpdate) throw errUpdate;
+
+                            resolve({ ok: 'Horario ' + modHorariosObj.horary + ' modificado correctamente' });
+                        });
+                    } else {
+                        resolve({ error: 'El horario ' + horary + ' no existe' });
+                    }
+                });
+            });
+        },
+        validate: {
+            payload: Joi.object().keys({
+                id: Joi.string(),
+                year: Joi.string(),
+                horary: Joi.string()
+            })
+        }
+    }
+}
+//fin modificar horarios
+
 
 ];
 export default administrationPanel;
