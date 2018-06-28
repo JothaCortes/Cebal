13a027dc270b6f208a8d98b5de372498b2f8de44
JothaCortes <j.cortesolivarez@gmail.com>
2018-06-28 15:50:47 -0400

    disabledAlumns

--- routes/api/enrolled.js
+++ routes/api/enrolled.js
@@ -7,7 +7,7 @@ let db = cloudant.db.use(configEnv.db)
 
 
 const Enrolled = [
-    //API TRAER ALUMNOS A TABLE
+    //API TRAER ALUMNOS ENROLLED A TABLE
 { 
     method: 'GET',
     path: '/api/studentsEnrolled', 
@@ -73,7 +73,108 @@ const Enrolled = [
             });
         }
     }
-}, 
+},    //API TRAER ALUMNOS DISABLED A TABLE
+{ 
+    method: 'GET',
+    path: '/api/studentsDisabled', 
+    options: {
+        handler: (request, h) => {
+            let credentials = request.auth.credentials;
+            return new Promise(resolve => {
+                db.find({
+                    'selector': {
+                        '_id': {
+                            '$gte': null
+                        },
+                        'city': credentials.place,
+                        'type': 'alumnos',
+                        'status': 'disabled'
+                    }
+                }, (err, result) => {
+                    if (err) throw err;
+
+                    if (result.docs[0]) {
+                        let res = result.docs.reduce((arr, el, i)=>{
+                            return arr.concat({
+                                _id: el._id,
+                                status: el.status,
+                                birthday:el.birthday,  
+                                name: el.name,
+                                lastname1: el.lastname1,
+                                lastname2: el.lastname2,
+                                email: el.email,
+                                phone: el.phone,
+                                address: el.address,
+                                nameAp: el.nameAp,
+                                relationshipAp: el.relationshipAp,
+                                workAp: el.workAp,
+                                phoneAp: el.phoneAp,
+                                city: el.city,
+
+                                colegio:el.matricula.colegio,
+                                añoEgreso:el.matricula.añoEgreso,
+                                promedio:el.matricula.promedio,
+                                horario:el.matricula.horario,
+                                electivo:el.matricula.electivo,
+                                electivo2:el.matricula.electivo2,
+                                apoderado: el.nameAp,
+                                parentesco: el.relationshipAp,
+                                workAp: el.workAp,
+                                phoneAp:el.phoneAp,
+                                date: el.matricula.date,
+                                tipoCurso: el.matricula.tipoCurso,
+                                formaP: el.matricula.finance.formaPago
+                            })
+                        }, []) 
+
+                        resolve(res);
+                    } else {
+                        resolve({ err: 'no existen alumnos' });
+                    }
+                });
+            });
+        }
+    }
+},
+{ // deshabilitar un alumno
+    method: 'DELETE',
+    path: '/api/deshabilitarAlumno',
+    options: {
+        handler: (request, h) => {
+            let id = request.payload.estudianteDes;
+            let studentData = {};
+  
+            return new Promise(resolve=>{
+              db.find({ 
+                "selector": {
+                    '_id': id,
+                    'type': 'alumnos',
+                    'status': 'enrolled'
+                },
+                "limit":1
+            }, function(err, result) {
+                if (err) throw err;
+  
+                studentData = result.docs[0];
+  
+                studentData.status = 'disabled';
+  
+                db.insert(studentData, function(errUpdate, body) {
+                    if (errUpdate) throw errUpdate;
+  
+                    resolve({ok: 'Estudiante '+ studentData.name +' deshabilitado correctamente'}); 
+                });  
+              });
+            }); 
+        },
+        validate: {
+            payload: Joi.object().keys({
+                estudianteDes: Joi.string()
+            })
+        }
+    }
+  }
+
 
 
 
