58ad72b36aec4245aa843bb21207a1962ac3f2df
Eduardo Reveco <eduardorevecovillalobos@gmail.com>
2018-06-29 12:47:53 -0400

    search

--- routes/api/tools.js
+++ routes/api/tools.js
@@ -18,9 +18,9 @@ const Tools = [{ // todos los clientes habilitados
   path: '/api/tools/uploadImgLog',
   options: {
     handler: (request, h) => {
-      let img = request.payload.img.replace(/^data:image\/jpeg;base64,/, "");
-      let id = request.payload.id;
-
+        let img = request.payload.img.replace(/^data:image\/jpeg;base64,/, "");
+        let id = request.payload.id;
+        
       return new Promise(resolve => {
         fs.writeFile(`img_logs/${id}.jpeg`, img, 'base64', function(err) {
           console.log(err);
@@ -36,7 +36,80 @@ const Tools = [{ // todos los clientes habilitados
       })
     }
   }
-}
+}, 
+{
+    method: 'POST',
+    path: '/api/tools/firstsearch',
+    options: {
+      handler: (request, h) => {
+        let type = request.payload.type;
+        let val = request.payload.val;
+        let query;
+
+        return new Promise(resolve=> {
+            if (type == 'email') {
+                query = {
+                    selector: {
+                        _id: {
+                            $gte: null
+                        },
+                        type: 'alumnos',
+                        email: val,
+                        $not: {
+                            status: 'disabled'
+                        }
+    
+                    }
+                }
+            } else if (type == 'rut') {
+                query = {
+                    selector: {
+                        _id: val
+                    },
+                    type: 'alumnos',
+                    $not: {
+                        status: 'disabled'
+                    }
+                }
+            } else {
+                let splitval = val.split(' ');
+    
+                let name = splitval[0];
+                let lastname = val.substr(val.indexOf(' ') + 1);
+    
+                query = {
+                    _id: {
+                        $gte: null
+                    },
+                    type: 'alumnos',
+                    $or: {
+                        name: {
+                            $regex: `(?i)${name}`
+                        },
+                        lastname: {
+                            $regex: `(?i)${lastname}`
+                        }  
+                    }     
+                }  
+            }
+
+            db.find(query, (err, result) => {
+                if (err) throw err
+      
+                console.log(result)
+                resolve(results)
+            })
+        })
+
+      },
+      validate: {
+        payload: Joi.object().keys({
+          type: Joi.string().required(),
+          val: Joi.string().required()
+        })
+      }
+    }
+}, 
 ];
 
 export default Tools
