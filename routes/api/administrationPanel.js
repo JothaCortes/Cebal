fb4467d93d0c91f66b47212df75e92ca667d1c6e
JothaCortes <j.cortesolivarez@gmail.com>
2018-07-05 16:04:40 -0400

    descuentosterminado

--- routes/api/administrationPanel.js
+++ routes/api/administrationPanel.js
@@ -30,7 +30,55 @@ const administrationPanel = [
         }
     }
 },
-//Api editar valores de cuotas
+//MODIFICAR VALORES DE CUOTAS
+{
+    method: 'POST',
+    path: '/api/modValoresCuotas',
+    options: {
+        handler: (request, h) => {
+            let status = request.payload.status;
+            let score = request.payload.score;
+            let price = request.payload.price;
+            let modValorObj = {};
+
+            return new Promise(resolve => {
+                db.find({
+                    "selector": {
+                        "_id": "quotaValue",
+                        //
+                        
+                    },
+                    "limit": 1
+                }, function (err, result) {
+                    if (err) throw err;
+
+                    if (result.docs[0]) {
+                        console.log(result)
+                        modValorObj = result.docs[0];
+                        modValorObj.score = score;
+                        modValorObj.price = price;
+
+                        db.insert(modValorObj, function (errUpdate, body) {
+                            if (errUpdate) throw errUpdate;
+
+                            resolve({ ok: 'Valor ' + modValorObj.price + ' de nota ' + modValorObj.score + ' modificado correctamente' });
+                        });
+                    } else {
+                        resolve({ error: 'El valor ' + price + ' no existe' });
+                    }
+                });
+            });
+        },
+        validate: {
+            payload: Joi.object().keys({
+                
+                status: Joi.string(),
+                score: Joi.string(),
+                price: Joi.string()
+            })
+        }
+    }
+},
 
 //xxxxx
 //Api Guardar Horarios
